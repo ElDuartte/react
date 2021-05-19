ac70868aeddb4fa28791c1cae6083ecea1190452
juan <el@duartte.co>
2021-05-19 22:31:56 +0200

    levantamiento de estado

--- platzi-badges/src/pages/BadgeNew.js
+++ platzi-badges/src/pages/BadgeNew.js
@@ -2,10 +2,30 @@ import React from "react";
 
 import "./styles/BadgeNew.css";
 import header from "../images/badge-header.svg";
-import Navbar from "../components/Navbar.js";
 import Badge from "../components/Badge.js";
+import BadgeForm from "../components/BadgeForm.js";
+import Navbar from "../components/Navbar.js";
 
 class BadgeNew extends React.Component {
+	state = {
+		form: {
+			firstName: "",
+			lastName: "",
+			email: "",
+			jobTitle: "",
+			twitter: "",
+		},
+	};
+
+	handleChange = (e) => {
+		this.setState({
+			form: {
+				...this.state.form,
+				[e.target.name]: e.target.value,
+			},
+		});
+	};
+
 	render() {
 		return (
 			<div>
@@ -15,13 +35,20 @@ class BadgeNew extends React.Component {
 				</div>
 				<div className="container">
 					<div className="row">
-						<div className="col">
+						<div className="col-6">
 							<Badge
-								firstName="Richard"
-								lastName="Sastre"
-								twitter="pepo"
-								jobTitle="Fontend"
+								firstName={this.state.form.firstName}
+								lastName={this.state.form.lastName}
+								twitter={this.state.form.twitter}
+								jobTitle={this.state.form.jobTitle}
+								email={this.state.form.email}
 							></Badge>
+						</div>
+						<div className="col-6">
+							<BadgeForm
+								onChange={this.handleChange}
+								formValues={this.state.form}
+							/>
 						</div>
 					</div>
 				</div>
