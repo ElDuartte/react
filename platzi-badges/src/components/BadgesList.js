a4d8a868c235077b85752a10af92c7093bee71e1
juan <el@duartte.co>
2021-06-02 22:45:34 +0200

    conectando con una api rest

--- platzi-badges/src/components/BadgesList.js
+++ platzi-badges/src/components/BadgesList.js
@@ -1,33 +1,57 @@
 import React from "react";
-import avatar from "../images/avatar.png";
+import { Link } from "react-router-dom";
 import "./styles/BadgesList.css";
+
+class BadgesListItem extends React.Component {
+	render() {
+		return (
+			<li key={this.props.badge.id} className="badgeList badgesContainer">
+				<img
+					className="BadgesListItem__avatar"
+					src={this.props.badge.avatarUrl}
+					alt={`${this.props.badge.firstName} ${this.props.badge.lastName}`}
+				/>
+
+				<div className="textBadges">
+					<h3 className="nameBadges">
+						{this.props.badge.firstName} {this.props.badge.lastName}
+					</h3>
+					<p className="infoBadges">
+						<br />
+						{this.props.badge.jobTitle}
+					</p>
+					<p className="socialBadges">@{this.props.badge.twitter}</p>
+				</div>
+			</li>
+		);
+	}
+}
 
 class BadgesList extends React.Component {
 	render() {
+		if (this.props.badges.length === 0) {
+			return (
+				<div>
+					<h3>No badges were found</h3>
+					<Link className="btn btn-primary" to="/Badges/new">
+						Create new badge
+					</Link>
+				</div>
+			);
+		}
+
 		return (
-			<ul className="list-unstyled">
-				{this.props.badges.map((badge) => {
-					return (
-						<li key={badge.id} className="badgeList badgesContainer">
-							<img className="avatarBadges" src={avatar} alt="avatar" />
-							<div className="textBadges">
-								<h3 className="nameBadges">
-									{badge.firstName}
-									{badge.lastName}
-									<br />
-								</h3>
-								<p className="infoBadges">
-									{badge.email}
-									<br />
-									{badge.jobTitle}
-									<br />
-								</p>
-								<p className="socialBadges">@{badge.twitter}</p>
-							</div>
-						</li>
-					);
-				})}
-			</ul>
+			<div className="BadgesList">
+				<ul className="list-unstyled">
+					{this.props.badges.map((badge) => {
+						return (
+							<li key={badge.id}>
+								<BadgesListItem badge={badge} />
+							</li>
+						);
+					})}
+				</ul>
+			</div>
 		);
 	}
 }
--- platzi-badges/src/pages/Badges.js
+++ platzi-badges/src/pages/Badges.js
@@ -1,75 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./styles/Badges.css";
 import confLogo from "../images/badge-header.svg";
 import BadgesList from "../components/BadgesList.js";
-import avatar from "../images/avatar.png";
-import { Link } from "react-router-dom";
+import api from "../api.js";
 
 class Badges extends React.Component {
-	constructor(props) {
-		super(props);
-		console.log("1. constructor()");
-
-		this.state = {
-			data: [],
-		};
-	}
+	state = {
+		loading: true,
+		error: null,
+		data: undefined,
+	};
 
 	componentDidMount() {
-		console.log("3. componentDidMount()");
-
-		this.timeoutId = setTimeout(() => {
-			this.setState({
-				data: [
-					{
-						id: "2de30c42-9deb-40fc-a41f-05e62b5939a7",
-						firstName: "Freda ",
-						lastName: "Grady",
-						email: "Leann_Berge@gmail.com",
-						jobTitle: "Legacy Brand Director",
-						twitter: "FredaGrady22221-7573",
-						avatar: { avatar },
-					},
-					{
-						id: "d00d3614-101a-44ca-b6c2-0be075aeed3d",
-						firstName: "Major ",
-						lastName: "Rodriguez",
-						email: "Ilene66@hotmail.com",
-						jobTitle: "Human Research Architect",
-						twitter: "ajorRodriguez61545",
-						avatar: { avatar },
-					},
-					{
-						id: "63c03386-33a2-4512-9ac1-354ad7bec5e9",
-						firstName: "Daphney ",
-						lastName: "Torphy",
-						email: "Ron61@hotmail.com",
-						jobTitle: "National Markets Officer",
-						twitter: "DaphneyTorphy96105",
-						avatar: { avatar },
-					},
-				],
-			});
-		}, 3000);
+		this.fetchData();
 	}
 
-	componentDidUpdate(prevProps, prevState) {
-		console.log("5. render()");
-		console.log({ prevProps: prevProps, prevState: prevState });
-		console.log({
-			props: this.props,
-			state: this.state,
-		});
-	}
+	fetchData = async () => {
+		this.setState({ loading: true, error: null });
 
-	componentWillUnmount() {
-		console.log("6. componentWillUnmount");
-		clearTimeout(this.timeoutId);
-	}
+		try {
+			const data = await api.badges.list();
+			this.setState({ loading: false, data: data });
+		} catch (error) {
+			this.setState({ loading: false, error: error });
+		}
+	};
 
 	render() {
-		console.log("2/4. render()");
+		if (this.state.loading === true) {
+			return "Loading...";
+		}
+
+		if (this.state.error) {
+			return `Error: ${this.state.error.message}`;
+		}
+
 		return (
 			<React.Fragment>
 				<div className="Badges">
