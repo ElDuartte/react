4583e1db0f20bb7bf3ff04c23e70f846671d3596
juan <el@duartte.co>
2021-06-04 12:40:50 +0200

    home, actualizacion de datos en las badges

--- platzi-badges/src/components/App.js
+++ platzi-badges/src/components/App.js
@@ -2,17 +2,21 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Layout from "./Layout.js";
+import Home from "../pages/Home.js";
 import Badges from "../pages/Badges.js";
 import BadgeNew from "../pages/BadgeNew.js";
-import NotFound from "./NotFound.js";
+import BadgeEdit from "../pages/BadgeEdit.js";
+import NotFound from "../pages/NotFound.js";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Layout>
 				<Switch>
+					<Route exact path="/" component={Home} />
 					<Route exact path="/badges" component={Badges} />
 					<Route exact path="/badges/new" component={BadgeNew} />
+					<Route exact path="/badges/:badgeId/edit" component={BadgeEdit} />
 					<Route component={NotFound} />
 				</Switch>
 			</Layout>
--- platzi-badges/src/pages/BadgeEdit.js
+++ platzi-badges/src/pages/BadgeEdit.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "./styles/BadgeNew.css";
+import "./styles/BadgeEdit.css";
 import header from "../images/platziconf-logo.svg";
 import Badge from "../components/Badge.js";
 import BadgeForm from "../components/BadgeForm.js";
@@ -8,9 +8,9 @@ import PageLoading from "../components/PageLoading.js";
 import api from "../api.js";
 import md5 from "md5";
 
-class BadgeNew extends React.Component {
+class BadgeEdit extends React.Component {
 	state = {
-		loading: false,
+		loading: true,
 		error: null,
 		form: {
 			firstName: "",
@@ -19,6 +19,22 @@ class BadgeNew extends React.Component {
 			jobTitle: "",
 			twitter: "",
 		},
+	};
+
+	componentDidMount() {
+		this.fetchData();
+	}
+
+	fetchData = async (e) => {
+		this.setState({ loading: true, error: null });
+
+		try {
+			const data = await api.badges.read(this.props.match.params.badgeId);
+
+			this.setState({ loading: false, form: data });
+		} catch (error) {
+			this.setState({ loading: false, error: error });
+		}
 	};
 
 	handleChange = (e) => {
@@ -40,7 +56,7 @@ class BadgeNew extends React.Component {
 		this.setState({ loading: true, error: null });
 
 		try {
-			await api.badges.create(this.state.form);
+			await api.badges.update(this.props.match.params.badgeId, this.state.form);
 			this.setState({ loading: false });
 
 			this.props.history.push("/badges");
@@ -55,9 +71,9 @@ class BadgeNew extends React.Component {
 		}
 		return (
 			<React.Fragment>
-				<div className="BadgeNew__hero">
+				<div className="BadgeEdit__hero">
 					<img
-						className="BadgeNew__hero-image img-fluid"
+						className="BadgeEdit__hero-image img-fluid"
 						src={header}
 						alt="Logo"
 					/>
@@ -75,7 +91,7 @@ class BadgeNew extends React.Component {
 							></Badge>
 						</div>
 						<div className="col-6">
-							<h1>New Attendant</h1>
+							<h1>Edit Attendant</h1>
 							<BadgeForm
 								onChange={this.handleChange}
 								onSubmit={this.handleSubmit}
@@ -90,4 +106,4 @@ class BadgeNew extends React.Component {
 	}
 }
 
-export default BadgeNew;
+export default BadgeEdit;
